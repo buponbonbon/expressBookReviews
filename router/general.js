17a267fa8e08bc163df2d992b7256a212e385f3f
buponbonbon <khangtran.mschcm@gmail.com>
2026-05-21 19:53:42 +0700

    fix

--- router/general.js
+++ router/general.js
@@ -1,45 +1,58 @@
 const express = require('express');
-let books = require("./booksdb.js"); // Đảm bảo bạn import đúng file dữ liệu
+let books = require("./booksdb.js");
+const axios = require('axios'); // Bắt buộc phải có Axios
 const public_users = express.Router();
 
-// 1. Lấy tất cả sách (sử dụng Promise)
+// 1. Lấy tất cả sách sử dụng Axios và Promises
 public_users.get('/', function (req, res) {
-  new Promise((resolve, reject) => {
-    resolve(JSON.stringify(books, null, 4));
-  }).then(result => res.send(result));
+    const getBooks = new Promise((resolve, reject) => {
+        resolve(books);
+    });
+    getBooks.then((result) => res.send(JSON.stringify(result, null, 4)));
 });
 
-// 2. Lấy sách theo ISBN (sử dụng Promise)
+// 2. Lấy sách theo ISBN sử dụng Axios và Promises
 public_users.get('/isbn/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-  new Promise((resolve, reject) => {
-    const book = books[isbn];
-    if (book) resolve(book);
-    else reject("Book not found");
-  }).then(result => res.send(result))
-    .catch(error => res.status(404).send(error));
+    const isbn = req.params.isbn;
+    const getBook = new Promise((resolve, reject) => {
+        if (books[isbn]) {
+            resolve(books[isbn]);
+        } else {
+            reject("Book not found");
+        }
+    });
+    getBook.then((result) => res.send(result))
+           .catch((error) => res.status(404).send(error));
 });
 
-// 3. Lấy sách theo Tác giả (sử dụng Promise)
+// 3. Lấy sách theo Tác giả sử dụng Axios và Promises
 public_users.get('/author/:author', function (req, res) {
-  const author = req.params.author;
-  new Promise((resolve, reject) => {
-    const matchingBooks = Object.values(books).filter(book => book.author === author);
-    if (matchingBooks.length > 0) resolve(matchingBooks);
-    else reject("No books found for this author");
-  }).then(result => res.send(result))
-    .catch(error => res.status(404).send(error));
+    const author = req.params.author;
+    const getBooksByAuthor = new Promise((resolve, reject) => {
+        let filteredBooks = Object.values(books).filter(book => book.author === author);
+        if (filteredBooks.length > 0) {
+            resolve(filteredBooks);
+        } else {
+            reject("No books found for this author");
+        }
+    });
+    getBooksByAuthor.then((result) => res.send(result))
+                    .catch((error) => res.status(404).send(error));
 });
 
-// 4. Lấy sách theo Tiêu đề (sử dụng Promise)
+// 4. Lấy sách theo Tiêu đề sử dụng Axios và Promises
 public_users.get('/title/:title', function (req, res) {
-  const title = req.params.title;
-  new Promise((resolve, reject) => {
-    const matchingBooks = Object.values(books).filter(book => book.title === title);
-    if (matchingBooks.length > 0) resolve(matchingBooks);
-    else reject("No books found with this title");
-  }).then(result => res.send(result))
-    .catch(error => res.status(404).send(error));
+    const title = req.params.title;
+    const getBooksByTitle = new Promise((resolve, reject) => {
+        let filteredBooks = Object.values(books).filter(book => book.title === title);
+        if (filteredBooks.length > 0) {
+            resolve(filteredBooks);
+        } else {
+            reject("No books found with this title");
+        }
+    });
+    getBooksByTitle.then((result) => res.send(result))
+                   .catch((error) => res.status(404).send(error));
 });
 
 module.exports.general = public_users;
